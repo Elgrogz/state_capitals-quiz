749ce3c8025380b78028e568468fd91acf261bc7
Elgrogz <gregorgilchrist@gmail.com>
2016-11-10 22:59:25 +0000

    removed original hash and tidied things up

--- us_state_capitals.rb
+++ us_state_capitals.rb
@@ -1,62 +1,3 @@
-# states= {
-#   'Alabama' => 'Montgomery',
-#   'Alaska' => 'Juno',
-#   'Arizona' => 'Phoenix',
-#   'Arkansas' => 'Little Rock',
-#   'California' => 'Sacramento',
-#   'Colorado' => 'Denver',
-#   'Connecticut' => 'Hartford',
-#   'Delaware' => 'Dover',
-#   'Florida' => 'Tallahassee',
-#   'Georgia' => 'Atlanta',
-#   'Hawaii' => 'Honolulu',
-#   'Idaho' => 'Boise',
-#   'Illinois' => 'Springfield',
-#   'Indiana' => 'Indianapolis',
-#   'Iowa' => 'Des Moines',
-#   'Kansas' => 'Topeka',
-#   'Kentucky' => 'Frankfort',
-#   'Louisiana' => 'Baton Rouge',
-#   'Maine' => 'Augusta',
-#   'Maryland' => 'Annapolis',
-#   'Massachusetts' => 'Boston',
-#   'Michigan' => 'Lansing',
-#   'Minnesota' => 'St. Paul',
-#   'Mississippi' => 'Jackson',
-#   'Missouri' => 'Jefferson City',
-#   'Montana' => 'Helena',
-#   'Nebraska' => 'Lincoln',
-#   'Nevada' => 'Carson City',
-#   'New Hampshire' => 'Concord',
-#   'New Jersey' => 'Trenton',
-#   'New Mexico' => 'Santa Fe',
-#   'New York' => 'Albany',
-#   'North Carolina' => 'Raleigh',
-#   'North Dakota' => 'Bismark',
-#   'Ohio' => 'Columbus',
-#   'Oklahoma' => 'Oklahoma City',
-#   'Oregon' => 'Salem',
-#   'Pennsylvania' => 'Harrisburg',
-#   'Rhode Island' => 'Providence',
-#   'South Carolina' => 'Columbia',
-#   'South Dakota' => 'Pierre',
-#   'Tennessee' => 'Nashville',
-#   'Texas' => 'Austin',
-#   'Utah' => 'Salt Lake City',
-#   'Vermont' => 'Montpelier',
-#   'Virginia' => 'Richmond',
-#   'Washington' => 'Olympia',
-#   'West Virginia' => 'Charleston',
-#   'Wisconsin' => 'Madison',
-#   'Wyoming' => 'Cheyenne'
-# }
-
-
-    @score_count = 0
-    @winner = false
-    @answered_questions = []
-
-
 @states = [
   ['Alabama',  'Montgomery'],
   ['Alaska', 'Juno'],
@@ -109,6 +50,10 @@
   ['Wisconsin', 'Madison'],
   ['Wyoming', 'Cheyenne']
 ]
+
+@score_count = 0
+@winner = false
+@answered_questions = []
 
 def question_pair(array)
   @pair = array.sample
